dc848732b4279fab58152c64e3687a9a81bb22d0
nkorim321-creator <nkorim321@gmail.com>
2026-05-12 22:13:04 +0600

    Update PowerFull.user.js

--- PowerFull.user.js
+++ PowerFull.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Protected Script
-// @version      1.14
+// @version      1.15
 // @description  Super Fast Password-Protected Loader (HIT Catcher Optimized)
 // @match        https://worker.mturk.com/*
 // @run-at       document-start
